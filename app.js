a2d17e8d846d4726a6a884d6aeef94e39996ce72
Tony-L-93 <tonyliendro@gmail.com>
2022-02-16 15:08:27 -0300

    It was added Attack and Special Attack functions

---
+++ app.js
@@ -0,0 +1,43 @@
+ function getRandomValue(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+const app = Vue.createApp({
+    data(){
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+            currentRound: 0,
+        };
+    },
+    computed: {
+        monsterBarStyles() {
+            return {width: this.monsterHealth + '%'};
+        },
+        playerBarStyles() {
+            return {width: this.playerHealth + '%'};
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound %3 !== 0;
+        }
+    },
+    methods: {
+        attackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(12, 5);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            const attackValue = getRandomValue(15, 8);
+            this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        }
+    }
+});
+
+app.mount('#game')
